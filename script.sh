48d84aae03f71b2a17674a9c0e9036409b65abc8
ankit rokde <ankitrokdeonsns@gmail.com>
2014-04-08 08:17:38 -0400

    moving shared files across systems to shared dir

--- script.sh
+++ script.sh
@@ -3,12 +3,12 @@
 OLD_DOTFILES_DIR=~/old_dotfiles
 mkdir -p $OLD_DOTFILES_DIR
 
-DOTFILES_DIR=~/dotfiles
+SHARED_FILES_DIR=~/dotfiles/shared
 
-FILES=`ls -A $DOTFILES_DIR`
+FILES=`ls -A $SHARED_FILES_DIR`
 
 for f in $FILES
 do
     mv ~/.$f $OLD_DOTFILES_DIR
-    ln -s $DOTFILES_DIR/$f ~/.$f
+    ln -s $SHARED_FILES_DIR/$f ~/.$f
 done
